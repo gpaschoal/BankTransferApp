ad4d47e8f7b6276a99504952f5551ee062c4bd91
Guilherme Paschoal <paschoal.gui@gmail.com>
2026-04-22 08:30:04 -0300

    create the repository interface

---
+++ Src/BankTransferApp.Domain/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using BankTransferApp.Domain.Entities;
+
+namespace BankTransferApp.Domain.Repositories;
+
+public interface IRepository<T>
+    where T : class, IEntity
+{
+    Task<T> GetByIdAsync(Guid id);
+
+    Task CreateAsync(T entity);
+
+    Task UpdateAync(T entity);
+
+    Task DeleteAsync(Guid id);
+}
