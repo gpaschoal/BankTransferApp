5c0ed71399f21d2419568c2a953174cbc100a793
Guilherme Paschoal <paschoal.gui@gmail.com>
2026-04-24 08:48:36 -0300

    add Queryable property to simplify methods

--- Src/BankTransferApp.Infrastructure/Repositories/RepositoryBase.cs
+++ Src/BankTransferApp.Infrastructure/Repositories/RepositoryBase.cs
@@ -1,28 +1,31 @@
 ﻿using BankTransferApp.Domain.Entities;
 using BankTransferApp.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankTransferApp.Infrastructure.Repositories;
 
 public abstract class RepositoryBase<T>(AppDbContext dbContext) : IRepository<T>
     where T : class, IEntity
 {
+    protected DbSet<T> Queryable => dbContext.Set<T>();
+
     public async Task CreateAsync(T entity, CancellationToken cancellationToken)
     {
-        await dbContext.Set<T>().AddAsync(entity, cancellationToken);
+        await Queryable.AddAsync(entity, cancellationToken);
     }
 
     public async Task DeleteAsync(T entity, CancellationToken cancellationToken)
     {
-        await dbContext.Set<T>().AddAsync(entity, cancellationToken);
+        await Queryable.AddAsync(entity, cancellationToken);
     }
 
-    public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    public async Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
-        return await dbContext.Set<T>().FindAsync([id], cancellationToken);
+        return await Queryable.FindAsync([id], cancellationToken);
     }
 
     public async Task UpdateAync(T entity, CancellationToken cancellationToken)
     {
-        await dbContext.Set<T>().AddAsync(entity, cancellationToken);
+        await Queryable.AddAsync(entity, cancellationToken);
     }
 }
