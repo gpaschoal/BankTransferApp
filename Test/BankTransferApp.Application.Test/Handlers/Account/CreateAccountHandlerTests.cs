80c281c45a94b43106de4dff133489385a8b7e1d
Guilherme Paschoal <paschoal.gui@gmail.com>
2026-04-26 10:30:29 -0300

    add test that check if throws an exception if user is not loggedin

--- Test/BankTransferApp.Application.Test/Handlers/Account/CreateAccountHandlerTests.cs
+++ Test/BankTransferApp.Application.Test/Handlers/Account/CreateAccountHandlerTests.cs
@@ -1,4 +1,5 @@
 ﻿using BankTransferApp.Application.Handlers.Account.CreateAccount;
+using BankTransferApp.Application.Service;
 using BankTransferApp.Domain.Enums;
 using BankTransferApp.Domain.Repositories;
 using BankTransferApp.Domain.Services;
@@ -36,6 +37,34 @@ public class CreateAccountHandlerTests
         result.Errors.ShouldNotBeEmpty();
 
         userContextServiceMock.Verify(x => x.ThrownsIfUserNotLoggedIn(), Times.Never);
+        userRepositoryMock.Verify(x => x.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Never);
+        unitOfWorkMock.Verify(x => x.BeginTransactionAsync(It.IsAny<CancellationToken>()), Times.Never);
+        accountRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Domain.Entities.AccountEntity>(), It.IsAny<CancellationToken>()), Times.Never);
+        unitOfWorkMock.Verify(x => x.CommitTransactionAsync(It.IsAny<CancellationToken>()), Times.Never);
+        unitOfWorkMock.Verify(x => x.RollbackTransactionAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [TestMethod(DisplayName = "Should Thrown An Exception If User Is Not Logged In")]
+    public async Task InvalidCommand_ShouldThrownAnExceptionIfUserIsNotLoggedIn()
+    {
+
+        var loggerMock = new Mock<ILogger<CreateAccountHandler>>();
+        var accountRepositoryMock = new Mock<IAccountRepository>();
+        var userRepositoryMock = new Mock<IUserRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var userContextService = new UserContextService();
+
+        var sut = new CreateAccountHandler(
+            loggerMock.Object,
+            accountRepositoryMock.Object,
+            userRepositoryMock.Object,
+            unitOfWorkMock.Object,
+            userContextService);
+
+        CreateAccountCommand command = new(EAccountType.CurrentAccount);
+
+        await sut.HandleAsync(command, CancellationToken.None).ShouldThrowAsync<InvalidOperationException>();
+
         userRepositoryMock.Verify(x => x.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Never);
         unitOfWorkMock.Verify(x => x.BeginTransactionAsync(It.IsAny<CancellationToken>()), Times.Never);
         accountRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Domain.Entities.AccountEntity>(), It.IsAny<CancellationToken>()), Times.Never);
