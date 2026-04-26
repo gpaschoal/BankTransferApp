b9af0599c1685def8144ea307b8265acfa95afeb
Guilherme Paschoal <paschoal.gui@gmail.com>
2026-04-26 07:54:29 -0300

    move enum to its own file

--- Src/BankTransferApp.Domain/Entities/AccountEntity.cs
+++ Src/BankTransferApp.Domain/Entities/AccountEntity.cs
@@ -1,4 +1,6 @@
-﻿namespace BankTransferApp.Domain.Entities;
+﻿using BankTransferApp.Domain.Enums;
+
+namespace BankTransferApp.Domain.Entities;
 
 public class AccountEntity : IEntity, IAuditedFields, IActivableEntity
 {
@@ -8,7 +10,7 @@ public class AccountEntity : IEntity, IAuditedFields, IActivableEntity
     public long AccountNumber { get; set; }
     public decimal Balance { get; set; }
     public bool IsActive { get; set; }
-    public AccountType AccountType { get; set; }
+    public EAccountType AccountType { get; set; }
     public DateTime CreatedAt { get; set; }
     public Guid CreatedById { get; set; }
     public UserEntity CreatedBy { get; set; }
@@ -18,20 +20,4 @@ public class AccountEntity : IEntity, IAuditedFields, IActivableEntity
     public DateTime? DeletedAt { get; set; }
     public Guid? DeletedById { get; set; }
     public UserEntity DeletedBy { get; set; }
-}
-
-public enum AccountType
-{
-    /// <summary>
-    ///  Conta corrente
-    /// </summary>
-    CurrentAccount,
-    /// <summary>
-    ///  Conta poupança
-    /// </summary>
-    SavingsAccount,
-    /// <summary>
-    ///  Conta salário
-    /// </summary>
-    PayrollAccount
 }
---
+++ Src/BankTransferApp.Domain/Enums/EAccountType.cs
@@ -0,0 +1,17 @@
+﻿namespace BankTransferApp.Domain.Enums;
+
+public enum EAccountType
+{
+    /// <summary>
+    ///  Conta corrente
+    /// </summary>
+    CurrentAccount,
+    /// <summary>
+    ///  Conta poupança
+    /// </summary>
+    SavingsAccount,
+    /// <summary>
+    ///  Conta salário
+    /// </summary>
+    PayrollAccount
+}
